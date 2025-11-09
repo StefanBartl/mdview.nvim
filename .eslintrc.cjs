bc355cc5014b16da5f1bd7f8d5e6f68c9fc76e76
lavalue <bartl.stefan@pm.me>
2025-11-09 14:49:30 +0100

    chore(eslint): enable TypeScript parser and plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,28 @@ module.exports = {
     es2022: true,
     browser: true
   },
-  extends: ['eslint:recommended', 'prettier'],
+	// Nötige devDependencies: npm install -D @typescript-eslint/parser @typescript-eslint/eslint-plugin
+  // TypeScript parser und plugin aktivieren
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: 'module'
+    sourceType: 'module',
+    // Typ-basierte Regeln, entkommentieren:
+    // project: ['./tsconfig.json', './tsconfig.server.json'],
+    // tsconfigRootDir: __dirname
   },
+
+  plugins: ['@typescript-eslint'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+
   rules: {
     'no-console': 'off'
-  }
+  },
+
+  ignorePatterns: ['dist', 'node_modules']
 };
